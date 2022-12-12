f96bdb4cc08b16cc56306a549604dd75549e6303
myelin0 <emi.veisllari@gmail.com>
2022-12-12 15:37:59 +0100

    Generating rhymes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React, {useState} from "react";
 import './App.css';
 
+const BASE_URL = `https://api.datamuse.com`
+
 function App() {
   const [word, setWord] = useState("");
-  type rhyme = {
-    word:string;
-    numSyllables:1;
-    score:Number;
-  }
-  const [rhymes, setRhymes] = useState([]);
+  const [rhyme, setRhyme] = useState([]);
   const handleFormSubmit = (e) => {
     e.preventDefault();
-    fetch(`https://api.datamuse.com/words?rel_rhy=fast`)
+    fetch(`${BASE_URL}/words?rel_rhy=${word}`)
     .then((Response)=> Response.json())
     .then(setRhyme);
+    console.log(setRhyme)
 
   }
 
@@ -32,6 +30,7 @@ function App() {
         ></input>
         <button>Submit</button>
       </form>
+      <ul key = {"index"}>{rhyme.map((e)=><li>{e.word}</li>)}</ul>
     </div>
   );
 }
